8e7780eadef5782c77d91a3e9d316d962dad4bf4
Schmoiger <32551110+Schmoiger@users.noreply.github.com>
2020-10-25 20:51:58 +0000

    Added last month view to home page

--- app/content.py
+++ app/content.py
@@ -8,8 +8,8 @@ from db_utils import get_rows
 def graphs():
     all_graphs = {
         'lastday': 'last_day_bar.svg',
-        'lastweek': 'last_week_pie.svg',
-        'lastmonth': 'last_month_bar.svg'
+        'lastweek': 'last_week_bar.svg',
+        'lastmonth': 'last_month_pie.svg'
     }
     return all_graphs
 
